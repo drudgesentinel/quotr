421df05f045db895a29385a869ebd4799a35de7d
Sean <sean.meininger@pm.me>
2021-01-20 13:14:23 -0800

    Better function naming

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
 const chalk = require('chalk');
 const quotes = require('./quotes.js');
 
-const getRandom = list => {
+const randomArrayItem = list => {
   return list[Math.floor((Math.random()*list.length))];
 }
 
-const randomQuote = getRandom(quotes);
+const randomQuote = randomArrayItem(quotes);
 console.log(`${chalk.blue(randomQuote.quote)}\n - ${chalk.cyan(randomQuote.author)}`);
