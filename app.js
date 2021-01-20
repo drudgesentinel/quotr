a33645b67a877f50e55ee0232bdd5bff9510a449
Sean Meininger <sean.meininger@pm.me>
2021-01-19 21:02:22 -0800

    Added quotes

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
+#!/usr/bin/env node
 const chalk = require('chalk');
+const quotes = require('./quotes.js');
+
+chalk.blue(console.log(quotes[0]));
---
+++ quotes.js
@@ -0,0 +1,14 @@
+const quotes = [
+  {"author": "Marcus Aurelius", "quote": "The soul becomes dyed with the color of its thoughts."},
+  {"author": "Epictetus", "quote": "Wealth consists not in having great possessions, but in having few wants."},
+  {"author": "Epictetus", "quote": "Working within our sphere of control, we are naturally free, independent, and strong. Beyond that sphere, we are weak, limited, and dependent."},
+  {"author": "Seneca", "quote": "They lose the day in expectation of the night, and the night in fear of the dawn."},
+  {"author": "Seneca", "quote": "If a man knows not to which port he sails, no wind is favorable."},
+  {"author": "Friedrich Nietzsche", "quote": "The surest way to corrupt a youth is to instruct him to hold in higher esteem those who think alike than those who think differently."},
+  {"author": "Friedrich Nietzsche", "quote": "In individuals, insanity is rare; but in groups, parties, nations and epochs, it is the rule."},
+  {"author": "Lucretius", "quote": "But if one should guide his life by true principles, man's greatest riches is to live on a little with contented mind; for a little is never lacking."},
+  {"author": "Lucretius", "quote": "A little river seems to him, who has never seen a larger river, a mighty stream; and so with other things—a tree, a man—anything appears greatest to him that never knew a greater."},
+  {"author": "Christopher Hitchens", "quote": "To the dumb question \"Why me?\" the cosmos barely bothers to return the reply: why not?"},
+  {"author": "Stephen Hawking", "quote": "“Remember to look up at the stars and not down at your feet. Never give up work. Work gives you meaning and purpose and life is empty without it. If you are lucky enough to find love, remember it is there and don't throw it away."},
+];
+module.exports = quotes;
